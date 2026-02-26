a96421f9018bbecd31e1d8821d34edba96cb65c7
Natalia Bernal <nataliamares25@gmail.com>
2026-02-26 14:55:48 -0500

    Funcion bitacora

---
+++ BitacoraService.cs
@@ -0,0 +1,27 @@
+﻿using CapaDatos;
+using CapaDatos.Modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CapaNegocio.Bitacora
+{
+    public class BitacoraService
+    {
+        private readonly BitacoraRepository _repo = new BitacoraRepository();
+
+        public Task LogAsync(
+            string modulo,
+            string accion,
+            string referencia,
+            string detalle,
+            bool exitoso,
+            string errorMensaje = null)
+        {
+            return _repo.RegistrarAsync(modulo, accion, referencia, detalle, exitoso, errorMensaje);
+        }
+
+        // (Opcional) para ver logs desde un grid admin
+        public Task<List<BitacoraEntry>> ObtenerUltimosAsync(int limite = 200)
+            => _repo.ObtenerUltimosAsync(limite);
+    }
+}
