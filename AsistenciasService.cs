4a4f9edb84f8bfd29ba346af97556933216ad8ba
Natalia Bernal <nataliamares25@gmail.com>
2026-02-26 14:55:13 -0500

    Funcion asistencia

---
+++ AsistenciasService.cs
@@ -0,0 +1,18 @@
+﻿using CapaDatos;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CapaNegocio.Asistencia
+{
+    public class AsistenciasService
+    {
+        // Repositorio para acceder a los datos de asistencias
+        private readonly AsistenciasRepository _repo = new AsistenciasRepository();
+
+        // Método para cargar las asistencias del día de hoy de forma asíncrona
+        public Task<DataTable> CargarHoyAsync()
+        {
+            return _repo.ObtenerAsistenciasDeHoyAsync();
+        }
+    }
+}
